49eb39389db0391bfa4cfb2b735a45d09e249860
azwickey-pivotal <vania12R>
2017-02-16 16:00:23 -0500

    added concourse task assets

--- completed-ci/tasks/package.sh
+++ completed-ci/tasks/package.sh
@@ -4,4 +4,4 @@ set -xe
 
 cd git-assets/concourse-workshop
 mvn package
-cp target/concourse-demo-*.jar ../../app-output/concourse-demo.jar
+cp target/concourse-demo-*.jar ../app-output/concourse-demo.jar
